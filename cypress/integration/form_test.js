22499bbc3486dbb6598f5a3f00ab5a373e7cad5f
Tusk617 <59673124+Tusk617@users.noreply.github.com>
2020-03-26 14:47:14 -0400

    Completed step 2, added all tests and assertion checks

---
+++ cypress/integration/form_test.js
@@ -0,0 +1,21 @@
+describe("Testing our inputs and submitting our form", function() {
+    beforeEach(function() {
+        cy.visit("http://localhost:3000/")
+    })
+    it("Adding tests to input", function() {
+        cy.get("input[name='name']")
+            .type("Kory Hacker")
+            .should("have.value", "Kory Hacker");
+        cy.get("input[name='email']")
+            .type("kory.hacker@gmail.com")
+            .should("have.value", "kory.hacker@gmail.com");
+        cy.get("input[name='password']")
+            .type("password123")
+            .should("have.value", "password123");
+        cy.get("select[name='role']")
+            .select("Mage")
+            .should("have.value", "Mage");
+        cy.get("input[name='terms']").check()
+            .should("be.checked");
+    })
+})
